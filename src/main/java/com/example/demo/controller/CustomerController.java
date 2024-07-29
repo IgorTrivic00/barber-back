2873c12b589c69ccf8ba5abb7648b1c6262936b9
Igor Trivic <trivicigor00@gmail.com>
2024-07-29 22:40:25 +0200

    [develop] Izmena korisnika

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,4 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Customer;
+import com.example.demo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/update")
+    public Customer update(@RequestBody Customer customer){
+        return customerService.update(customer);
+    }
+
 }
--- src/main/java/com/example/demo/dto/Customer.java
+++ src/main/java/com/example/demo/dto/Customer.java
@@ -6,10 +6,11 @@ import java.util.UUID;
 public record Customer(Optional<Long> id,
                        Optional<UUID> uuid,
                        String name,
+                       Optional<String> mobile,
                        User user) {
 
-    public Customer(String name, User user){
-        this(Optional.empty(), Optional.empty(), name, user);
+    public Customer(String name, String mobile, User user){
+        this(Optional.empty(), Optional.empty(), name, Optional.of(mobile), user);
     }
 
 }
